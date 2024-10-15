b7d801485e9a1a6943412deb00b57b9edd91a75c
arianakhbari <77466277+arianakhbari@users.noreply.github.com>
2024-10-15 14:35:30 -0700

    config.py

--- lira/config.py
+++ lira/config.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS").split(',')))  # فرض بر این است که ادمین‌ها با کاما جدا شده‌اند
-DATABASE_URL = os.getenv("DATABASE_URL")
+BOT_TOKEN = '7732549586:AAH3XpSaeY8m3BTzhCVZGlEJzwGz-okLmos'
+ADMIN_IDS = [179044957]
+DATABASE_URL = 'DATABASE_URL'

72670b210fdea4c49aeddeb26d361881db5fbd1e
arianakhbari <77466277+arianakhbari@users.noreply.github.com>
2024-10-15 14:43:59 -0700

    config.py

--- lira/config.py
+++ lira/config.py
@@ -1,10 +1,5 @@
 # config.py
 
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 BOT_TOKEN = '7732549586:AAH3XpSaeY8m3BTzhCVZGlEJzwGz-okLmos'
 ADMIN_IDS = [179044957]
 DATABASE_URL = 'DATABASE_URL'

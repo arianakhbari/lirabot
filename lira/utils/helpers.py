fb17168135349b4567fa1a1e603f402237eda7b2
arianakhbari <77466277+arianakhbari@users.noreply.github.com>
2024-10-15 14:49:04 -0700

    helpers.py

--- lira/utils/helpers.py
+++ lira/utils/helpers.py
@@ -1,11 +1,6 @@
 # utils/helpers.py
 
 from config import ADMIN_IDS
-from services.user_service import get_user_by_telegram_id
 
 def is_admin(user_id):
     return user_id in ADMIN_IDS
-
-def get_user_by_id(user_id):
-    # فرض بر این است که user_id همان telegram_id است
-    return get_user_by_telegram_id(user_id)

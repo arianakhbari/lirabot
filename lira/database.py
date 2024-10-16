959fd6ec8a5290979e908a6f204cc92ef9d38d71
arianakhbari <77466277+arianakhbari@users.noreply.github.com>
2024-10-16 11:03:38 -0700

    database.py

--- lira/database.py
+++ lira/database.py
@@ -5,7 +5,10 @@ from sqlalchemy.orm import sessionmaker
 from config import DATABASE_URL
 from models import Base
 
+# ایجاد engine
 engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})  # اگر از SQLite استفاده می‌کنید
+
+# ایجاد کلاس Session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # ایجاد جداول در پایگاه داده
